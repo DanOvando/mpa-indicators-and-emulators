8c91f59593f714ec8193ba51d0d620c263055f87
Dan Ovando <dovando07@gmail.com>
2024-11-21 11:06:49 -0800

    adding in observation error

--- 03_run_indicators.R
+++ 03_run_indicators.R
@@ -7,8 +7,8 @@ foos <- list.files(here::here("R"))
 purrr::walk(foos, ~ source(here::here("R", .x)))
 
 prep_run(
-  n_states = 84,
-  run_name = "indicators_v0.11",
+  n_states = 4,
+  run_name = "indicators_test",
   drop_patches = FALSE,
   experiment_workers = 8,
   rx = 20,
@@ -66,7 +66,7 @@ baseline_state_experiments <-
   tibble(
     kiss = sample(c(FALSE, TRUE), n_states, replace = TRUE),
     mpa_response = sample(c("stay", "leave"), n_states, replace = TRUE),
-    habitat_patchiness = runif(n_states, 1e-3, .2),
+    habitat_patchiness = runif(n_states, 1e-3, .1),
     max_abs_cor = runif(n_states, 1e-3, 1),
     spatial_q = sample(
       c(TRUE, FALSE),
@@ -178,34 +178,47 @@ for (difficulty in difficulties) {
   message("finished critters")
   
   
-  # aggregate into lists of fauna
+  # function to randomize selectivity parameters
+  selfoo <- function(x,i){
+    out <-   list(
+      sel_start = runif(i, .05, 2),
+      # proportion of length 50% mature at 50% selectivity
+      sel_delta = runif(i, 1e-3, .25),
+      # offset for 95% selectivity
+      sel05_anchor = runif(i, 0, 0.9),
+      sel_at_linf = runif(i, 0, 1)
+    )
+    
+    out$sel05_anchor <- out$sel05_anchor * out$sel_start # must be smaller than length at 50% selectivity
+    
+    return(out)
+    
+  }
   state_experiments <- state_experiments %>%
     group_by(state_id) %>%
     nest() %>%
     mutate(
       fauna = map(data, ~ .x$critter %>% set_names(.x$scientific_name)),
-      sels = map(fauna, ~ runif(length(.x), 0.1, 1.25)),
-      prices = map(fauna, ~ runif(length(.x), 1, 10))
-    )
-  
+      sels = map(fauna, ~ map2(1:2, length(.x), selfoo)),
+      sel_form = map(fauna, ~ map2(1:2,length(.x), ~sample(c("uniform","logistic","double_normal"), .y, replace = TRUE))),
+      prices = map(fauna, ~ map2(1:2, length(.x),~runif(.y, 1, 10))),
+      use_ports = map(fauna,~sample(c(TRUE,FALSE), 2, replace = TRUE))
+    ) |> 
+    ungroup()
   # state_experiments$fauna[[1]]$`lutjanus malabaricus`$diffusion_foundation[[1]] |> image()
   
   # state_experiments$fauna[[1]]$`lutjanus malabaricus`$movement_matrix[[1]] |> image()
   
   # state_experiments$fauna[[1]][[4]]$b0
   # stop()
-  
-    message("making fleets")
   state_experiments <- state_experiments %>%
-    ungroup() %>%
-    mutate(use_ports = sample(c(TRUE, FALSE), n(), replace = TRUE),
-           max_abs_cor_rec = sample(c(0,.66), n(), replace = TRUE)) %>%
     mutate(
-      fleet = future_pmap(
+      fleet = pmap(
         list(
           fauna = fauna,
           state = data,
           sels = sels,
+          sel_form = sel_form,
           prices = prices,
           use_ports = use_ports
         ),
@@ -213,13 +226,18 @@ for (difficulty in difficulties) {
         difficulty = difficulty,
         port_locations = port_locations,
         resolution = resolution,
-        .progress = TRUE,
-        .options = furrr_options(seed = TRUE)
-      ),
-      rec_dev_cov_and_cor = map2(fauna, max_abs_cor_rec, create_rec_dev_cov_and_cor)
+        .progress = "making fleets"
+      )
     )
-  message("finished making fleets")
   
+    # prepare recruitment deviate generator
+    state_experiments <- state_experiments |>
+      mutate(
+        max_abs_cor_rec = sample(c(0, .66), n(), replace = TRUE),
+        rec_dev_cov_and_cor = map2(fauna, max_abs_cor_rec, create_rec_dev_cov_and_cor)
+      )
+    
+ 
 
   # add in starting conditions
   init_condit <- function(fauna, fleets, rec_dev_cov_and_cor, years = 125) {
